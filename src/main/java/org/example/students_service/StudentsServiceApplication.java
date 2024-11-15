26a75beb4219fce588e4607ec5243339668d1621
Demerchyan Georgiy <Georgi747d@gmail.com>
2024-11-15 14:57:06 +0300

    Initial commit

---
+++ src/main/java/org/example/students_service/StudentsServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.students_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsServiceApplication.class, args);
+	}
+
+}
